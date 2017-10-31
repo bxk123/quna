496219e3d17c5b8094f69fb7d0ea8c7ae7ded649
bxk123 <2114319576@qq.com>
2017-10-31 23:03:01 +0800

    change

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+	@import "./reset.css";
+</style>
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+ 	<home-header/>
+</template>
+
+<script>
+
+import HeaderComponent from "./Header";
+
+export default {
+	components: {
+		"home-header": HeaderComponent
+	}
+}
+</script>
+
+<style>
+</style>
